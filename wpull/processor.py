cd86da17aaa9347de7b01294a58bb018c51d82b8
Christopher Foo <chris.foo@gmail.com>
2014-01-18 12:56:32 -0500

    Moves RobotsTxtSubsession into robotstxt module.

--- wpull/processor.py
+++ wpull/processor.py
@@ -9,7 +9,8 @@ from wpull.database import Status
 from wpull.errors import (ProtocolError, ServerError, ConnectionRefused,
     DNSNotFound)
 from wpull.http import Request, Response
-from wpull.robotstxt import RobotsTxtPool
+from wpull.robotstxt import (RobotsTxtPool, RobotsTxtSubsession,
+    RobotsTxtNotLoaded)
 from wpull.stats import Statistics
 from wpull.url import URLInfo
 from wpull.waiter import LinearWaiter
@@ -17,9 +18,6 @@ from wpull.waiter import LinearWaiter
 
 _logger = logging.getLogger(__name__)
 _ = gettext.gettext
-REDIRECT_STATUS_CODES = (301, 302, 303, 307, 308)
-DOCUMENT_STATUS_CODES = (200, 206)
-NO_DOCUMENT_STATUS_CODES = (401, 403, 404, 405, 410,)
 
 
 class BaseProcessor(object, metaclass=abc.ABCMeta):
@@ -71,6 +69,10 @@ class BaseProcessorSession(object, metaclass=abc.ABCMeta):
 
 
 class WebProcessor(BaseProcessor):
+    REDIRECT_STATUS_CODES = (301, 302, 303, 307, 308)
+    DOCUMENT_STATUS_CODES = (200, 206)
+    NO_DOCUMENT_STATUS_CODES = (401, 403, 404, 405, 410,)
+
     def __init__(self, url_filters=None, document_scrapers=None,
     file_writer=None, waiter=None, statistics=None, request_factory=None,
     retry_connrefused=False, retry_dns_error=False, max_redirects=20,
@@ -116,97 +118,6 @@ class WebProcessor(BaseProcessor):
         self._statistics.stop()
 
 
-class RobotsTxtNotLoaded(Exception):
-    pass
-
-
-class RobotsTxtSubsession(object):
-    class Result(object):
-        done = 1
-        redirect = 2
-        retry = 3
-        fail = 4
-
-    def __init__(self, robots_txt_pool, request_factory):
-        self._robots_txt_pool = robots_txt_pool
-        self._request = None
-        self._attempts_remaining = 20
-        self._redirects_remaining = 5
-        self._redirect_url = None
-        self._request_factory = request_factory
-
-    def can_fetch(self, request):
-        if not self._robots_txt_pool.has_parser(request):
-            raise RobotsTxtNotLoaded()
-
-        if not self._robots_txt_pool.can_fetch(request):
-            _logger.debug('Cannot fetch {url} due to robots.txt'.format(
-                url=request.url_info.url))
-            return False
-        else:
-            return True
-
-    def rewrite_request(self, request):
-        url_info = request.url_info
-
-        if self._redirect_url:
-            self._request = self._request_factory(self._redirect_url)
-            self._redirect_url = None
-        else:
-            url = URLInfo.parse('{0}://{1}:{2}/robots.txt'.format(
-                url_info.scheme, url_info.hostname, url_info.port)).url
-            self._request = self._request_factory(url)
-
-        return self._request
-
-    def check_response(self, response):
-        if self._attempts_remaining == 0:
-            _logger.warning(_('Too many failed attempts to get robots.txt.'))
-            return self.Result.fail
-
-        if self._redirects_remaining == 0:
-            _logger.warning(_('Ignoring robots.txt redirect loop.'))
-            return self.Result.done
-
-        if not response or 500 <= response.status_code <= 599:
-            self._attempts_remaining -= 1
-            return self.Result.retry
-        elif response.status_code in REDIRECT_STATUS_CODES:
-            self._accept_empty(self._request.url_info)
-            self._redirects_remaining -= 1
-            redirect_url = response.fields.get('location')
-            return self._parse_redirect_url(redirect_url)
-        else:
-            self._attempts_remaining = 20
-            self._redirects_remaining = 5
-            if response.status_code == 200:
-                self._accept_ok(response)
-                return self.Result.done
-            else:
-                self._accept_empty(self._request.url_info)
-                return self.Result.done
-
-    def _accept_ok(self, response):
-        try:
-            self._robots_txt_pool.load_robots_txt(self._request.url_info,
-                response.body.content_segment())
-        except ValueError:
-            _logger.warning(_('Failed to parse {url}. Ignoring.').format(
-                self._request.url_info.url))
-            self._accept_empty(self._request.url_info)
-
-    def _accept_empty(self, url_info):
-        self._robots_txt_pool.load_robots_txt(url_info, '')
-
-    def _parse_redirect_url(self, url):
-        if url:
-            self._redirect_url = url
-            return self.Result.redirect
-
-        self._accept_empty(self._request.url_info)
-        return self.Result.done
-
-
 class WebProcessorSession(BaseProcessorSession):
     class State(object):
         normal = 1
@@ -229,9 +140,9 @@ class WebProcessorSession(BaseProcessorSession):
         self._retry_dns_error = retry_dns_error
         self._redirects_remaining = max_redirects
         self._state = self.State.normal
-        self._redirect_codes = REDIRECT_STATUS_CODES
-        self._document_codes = DOCUMENT_STATUS_CODES
-        self._no_document_codes = NO_DOCUMENT_STATUS_CODES
+        self._redirect_codes = WebProcessor.REDIRECT_STATUS_CODES
+        self._document_codes = WebProcessor.DOCUMENT_STATUS_CODES
+        self._no_document_codes = WebProcessor.NO_DOCUMENT_STATUS_CODES
         self._url_record_status = None
 
         if robots_txt_pool:
@@ -422,5 +333,3 @@ class WebProcessorSession(BaseProcessorSession):
         _logger.debug('Found URLs: inline={0} linked={1}'.format(
             len(self._inline_urls), len(self._linked_urls)
         ))
-
-
--- wpull/robotstxt.py
+++ wpull/robotstxt.py
@@ -1,4 +1,12 @@
+import gettext
+import logging
 import robotexclusionrulesparser
+
+from wpull.url import URLInfo
+
+
+_logger = logging.getLogger(__name__)
+_ = gettext.gettext
 
 
 class RobotsTxtPool(object):
@@ -26,3 +34,96 @@ class RobotsTxtPool(object):
     @classmethod
     def url_info_key(cls, url_info):
         return (url_info.scheme, url_info.hostname, url_info.port)
+
+
+class RobotsTxtNotLoaded(Exception):
+    pass
+
+
+class RobotsTxtSubsession(object):
+    class Result(object):
+        done = 1
+        redirect = 2
+        retry = 3
+        fail = 4
+
+    def __init__(self, robots_txt_pool, request_factory,
+    redirect_status_codes=(301, 302, 303, 307, 308)):
+        self._robots_txt_pool = robots_txt_pool
+        self._request = None
+        self._attempts_remaining = 20
+        self._redirects_remaining = 5
+        self._redirect_url = None
+        self._request_factory = request_factory
+        self._redirect_status_codes = redirect_status_codes
+
+    def can_fetch(self, request):
+        if not self._robots_txt_pool.has_parser(request):
+            raise RobotsTxtNotLoaded()
+
+        if not self._robots_txt_pool.can_fetch(request):
+            _logger.debug('Cannot fetch {url} due to robots.txt'.format(
+                url=request.url_info.url))
+            return False
+        else:
+            return True
+
+    def rewrite_request(self, request):
+        url_info = request.url_info
+
+        if self._redirect_url:
+            self._request = self._request_factory(self._redirect_url)
+            self._redirect_url = None
+        else:
+            url = URLInfo.parse('{0}://{1}:{2}/robots.txt'.format(
+                url_info.scheme, url_info.hostname, url_info.port)).url
+            self._request = self._request_factory(url)
+
+        return self._request
+
+    def check_response(self, response):
+        if self._attempts_remaining == 0:
+            _logger.warning(_('Too many failed attempts to get robots.txt.'))
+            return self.Result.fail
+
+        if self._redirects_remaining == 0:
+            _logger.warning(_('Ignoring robots.txt redirect loop.'))
+            return self.Result.done
+
+        if not response or 500 <= response.status_code <= 599:
+            self._attempts_remaining -= 1
+            return self.Result.retry
+        elif response.status_code in self._redirect_status_codes:
+            self._accept_empty(self._request.url_info)
+            self._redirects_remaining -= 1
+            redirect_url = response.fields.get('location')
+            return self._parse_redirect_url(redirect_url)
+        else:
+            self._attempts_remaining = 20
+            self._redirects_remaining = 5
+            if response.status_code == 200:
+                self._accept_ok(response)
+                return self.Result.done
+            else:
+                self._accept_empty(self._request.url_info)
+                return self.Result.done
+
+    def _accept_ok(self, response):
+        try:
+            self._robots_txt_pool.load_robots_txt(self._request.url_info,
+                response.body.content_segment())
+        except ValueError:
+            _logger.warning(_('Failed to parse {url}. Ignoring.').format(
+                self._request.url_info.url))
+            self._accept_empty(self._request.url_info)
+
+    def _accept_empty(self, url_info):
+        self._robots_txt_pool.load_robots_txt(url_info, '')
+
+    def _parse_redirect_url(self, url):
+        if url:
+            self._redirect_url = url
+            return self.Result.redirect
+
+        self._accept_empty(self._request.url_info)
+        return self.Result.done
