89255d8191fa0381bfc743934414f038bfb4810c
Christopher Foo <chris.foo@gmail.com>
2014-03-31 01:59:56 -0400

    setup.py: Moves setup args into a dict.

--- setup.py
+++ setup.py
@@ -28,64 +28,65 @@ if sys.version_info[0] == 3:
     config_section = 'src_package_dir'
 else:
     config_section = 'dst_package_dir'
+
 PROJECT_PACKAGES = config.options(config_section)
 PROJECT_PACKAGE_DIR = dict(config.items(config_section))
 
 
-extras = {}
-install_requires = [
+setup_kwargs = dict(
+    name='wpull',
+    version=version,
+    description='Wget-compatible web downloader.',
+    author='Christopher Foo',
+    author_email='chris.foo@gmail.com',
+    url='https://github.com/chfoo/wpull',
+    package_data={'': [
+        'cert/ca-bundle.pem',
+        'testing/*/*.css',
+        'testing/*/*.html',
+        'testing/*/*.txt',
+        'testing/*/*.lua',
+        'testing/*/*.rst',
+        'testing/*/*.js',
+        'testing/*/*.png',
+        'testing/*/*.xml',
+        'testing/*.lua',
+        '*.js',
+    ]},
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: System :: Archiving',
+    ],
+    packages=PROJECT_PACKAGES,
+    package_dir=PROJECT_PACKAGE_DIR,
+)
+
+setup_kwargs['install_requires'] = [
     'tornado', 'toro', 'lxml', 'chardet', 'sqlalchemy', 'beautifulsoup4',
     'namedlist',
 ]
 
 if sys.version_info[0] == 2:
-    install_requires.append('futures')
+    setup_kwargs['install_requires'].append('futures')
     # Requiring 3to2 doesn't mean it will be installed first
     # Also it might cause confusion by downloading from PyPI which
     # we do not want.
     # install_requires.append('3to2')
-    scripts = ['scripts/wpull', 'scripts/wpull2']
+    setup_kwargs['scripts'] = ['scripts/wpull', 'scripts/wpull2']
 else:
-    scripts = ['scripts/wpull', 'scripts/wpull3']
+    setup_kwargs['scripts'] = ['scripts/wpull', 'scripts/wpull3']
 
 
 if __name__ == '__main__':
     if sys.version_info[0] == 2:
         backport.translate_project('backport.conf')
 
-    setup(name='wpull',
-        version=version,
-        description='Wget-compatible web downloader.',
-        author='Christopher Foo',
-        author_email='chris.foo@gmail.com',
-        url='https://github.com/chfoo/wpull',
-        package_data={'': [
-            'cert/ca-bundle.pem',
-            'testing/*/*.css',
-            'testing/*/*.html',
-            'testing/*/*.txt',
-            'testing/*/*.lua',
-            'testing/*/*.rst',
-            'testing/*/*.js',
-            'testing/*/*.png',
-            'testing/*/*.xml',
-            'testing/*.lua',
-            '*.js',
-        ]},
-        install_requires=install_requires,
-        classifiers=[
-            'Development Status :: 4 - Beta',
-            'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-            'Programming Language :: Python :: 2',
-            'Programming Language :: Python :: 2.6',
-            'Programming Language :: Python :: 2.7',
-            'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.2',
-            'Programming Language :: Python :: 3.3',
-            'Topic :: Internet :: WWW/HTTP',
-            'Topic :: System :: Archiving',
-        ],
-        packages=PROJECT_PACKAGES,
-        package_dir=PROJECT_PACKAGE_DIR,
-        scripts=scripts,
-)
+    setup(**setup_kwargs)
