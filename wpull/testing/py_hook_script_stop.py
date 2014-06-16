08f7991738b572be653739e360222a0468c516af
Christopher Foo <chris.foo@gmail.com>
2014-06-16 20:30:41 +0800

    testing/py_hook_script_stop.py: Add missing record_info arg.

--- wpull/testing/py_hook_script_stop.py
+++ wpull/testing/py_hook_script_stop.py
@@ -4,7 +4,7 @@
 wpull_hook = globals().get('wpull_hook')  # silence code checkers
 
 
-def handle_response(url_info, http_info):
+def handle_response(url_info, record_info, http_info):
     print('handle_response', url_info)
 
     return wpull_hook.actions.STOP
