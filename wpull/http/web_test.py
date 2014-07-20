5acec712f26fa1a30ebabd3a083a3ae92cdba7cc
Christopher Foo <chris.foo@gmail.com>
2014-07-20 21:57:32 +0800

    WIP Fix up .http.redirect unit tests
    [ci skip]

--- wpull/http/web_test.py
+++ wpull/http/web_test.py
@@ -13,53 +13,6 @@ from wpull.testing.goodapp import GoodAppTestCase
 DEFAULT_TIMEOUT = 30
 
 
-class TestWeb(unittest.TestCase):
-    def test_redirect_tracker(self):
-        tracker = RedirectTracker(5)
-
-        self.assertFalse(tracker.is_redirect())
-        self.assertFalse(tracker.is_repeat())
-        self.assertFalse(tracker.exceeded())
-        self.assertFalse(tracker.next_location(raw=True))
-        self.assertEqual(0, tracker.count())
-
-        response = Response('HTTP/1.1', 200, '')
-
-        tracker.load(response)
-
-        self.assertFalse(tracker.is_redirect())
-        self.assertFalse(tracker.is_repeat())
-        self.assertFalse(tracker.exceeded())
-        self.assertFalse(tracker.next_location())
-        self.assertEqual(0, tracker.count())
-
-        response = Response('HTTP/1.1', 303, '')
-        response.fields['location'] = '/test'
-
-        tracker.load(response)
-
-        self.assertTrue(tracker.is_redirect())
-        self.assertFalse(tracker.is_repeat())
-        self.assertFalse(tracker.exceeded())
-        self.assertEqual('/test', tracker.next_location(raw=True))
-        self.assertEqual(1, tracker.count())
-
-        response = Response('HTTP/1.1', 307, '')
-        response.fields['location'] = '/test'
-
-        tracker.load(response)
-        tracker.load(response)
-        tracker.load(response)
-        tracker.load(response)
-        tracker.load(response)
-
-        self.assertTrue(tracker.is_redirect())
-        self.assertTrue(tracker.is_repeat())
-        self.assertTrue(tracker.exceeded())
-        self.assertEqual('/test', tracker.next_location(raw=True))
-        self.assertEqual(6, tracker.count())
-
-
 class TestRichClient(GoodAppTestCase):
     @tornado.testing.gen_test(timeout=DEFAULT_TIMEOUT)
     def test_basic(self):
