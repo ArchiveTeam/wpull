ArchiveTeam/wpull

commit 342acf5368d0b2fc484b4cf3e5e44fb390b9ab4a

scaper.javascript: Try JSON decode before is_unlikely_text

wpull/scraper/javascript.py
@@ -25,12 +25,17 @@ class JavaScriptScraper(JavaScriptReader, BaseTextStreamScraper):
 
     def iter_text(self, file, encoding=None):
         for text, match in super().iter_text(file, encoding):
-            if match and is_likely_link(text) and \
-                    not is_unlikely_link(text):
+            if match:
                 try:
-                    yield (json.loads('"{0}"'.format(text)), match)
+                    new_text = json.loads('"{0}"'.format(text))
                 except ValueError:
                     yield (text, match)
+                else:
+                    if is_likely_link(new_text) and \
+                            not is_unlikely_link(new_text):
+                        yield (new_text, match)
+                    else:
+                        yield (text, None)
             else:
                 yield (text, None)
 

commit cc4255ef5d79eb9fc8c9f580d1593d3ca9657a59

scraper.javascript: Fix nonsensical link scraping code
Regression introduced in 342acf5368d0b2fc484b4cf3e5e44fb390b9ab4a and
made worse by 342acf5368d0b2fc484b4cf3e5e44fb390b9ab4a

Closes chfoo/wpull#192

wpull/scraper/javascript.py
@@ -9,7 +9,7 @@ from wpull.document.javascript import JavaScriptReader
 from wpull.document.util import detect_response_encoding
 from wpull.scraper.base import BaseTextStreamScraper
 from wpull.scraper.util import is_likely_inline, is_likely_link, \
-    is_unlikely_link
+    is_unlikely_link, urljoin_safe
 import wpull.util
 
 
@@ -23,21 +23,31 @@ class JavaScriptScraper(JavaScriptReader, BaseTextStreamScraper):
         super().__init__()
         self._encoding_override = encoding_override
 
-    def iter_text(self, file, encoding=None):
-        for text, match in super().iter_text(file, encoding):
-            if match:
+    def iter_processed_text(self, file, encoding=None, base_url=None):
+        for text, is_link in self.iter_text(file, encoding):
+            if is_link:
                 try:
                     new_text = json.loads('"{0}"'.format(text))
                 except ValueError:
-                    yield (text, match)
+                    yield (text, False)
+                    continue
+
+                if is_unlikely_link(new_text) or not is_likely_link(new_text):
+                    yield (text, False)
+                    continue
+
+                if base_url:
+                    new_link = urljoin_safe(base_url, new_text,
+                                            allow_fragments=False)
                 else:
-                    if is_likely_link(new_text) and \
-                            not is_unlikely_link(new_text):
-                        yield (new_text, match)
-                    else:
-                        yield (text, None)
+                    new_link = new_text
+
+                if new_link:
+                    yield (new_link, True)
+                else:
+                    yield (text, False)
             else:
-                yield (text, None)
+                yield (text, False)
 
     def scrape(self, request, response):
         if not self.is_supported(request=request, response=response):

wpull/testing/samples/script.js
@@ -16,3 +16,8 @@ link = {
 };
 var size = "4.0";
 var mimetype = 'image/png';
+link = "horse dolphin";
+link = "cat[]monkey";
+link = "fish$dog"
+link = "rabbit\\ferre\t"
+link = '\u9'