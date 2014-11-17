ArchiveTeam/wpull

commit 38a41521f6dcd658165313a83abe95490a6dc169

cookie: Limit cookie length per domain.
Closes chfoo/wpull#200

wpull/cookie.py
@@ -23,9 +23,11 @@ class DeFactoCookiePolicy(DefaultCookiePolicy):
         if not DefaultCookiePolicy.set_ok(self, cookie, request):
             return False
 
-        cookie_string = '{}={}'.format(cookie.name, cookie.value)
+        new_cookie_length = (self.cookie_length(cookie.domain) +
+                             len(cookie.path) + len(cookie.name) +
+                             len(cookie.value))
 
-        if len(cookie_string) > 4100:
+        if new_cookie_length >= 4100:
             return False
 
         if self.count_cookies(cookie.domain) >= 50:
@@ -35,7 +37,7 @@ class DeFactoCookiePolicy(DefaultCookiePolicy):
             except KeyError:
                 return False
 
-        if not wpull.util.is_ascii(cookie_string):
+        if not wpull.util.is_ascii(str(cookie)):
             return False
 
         return True
@@ -50,6 +52,22 @@ class DeFactoCookiePolicy(DefaultCookiePolicy):
             )
         else:
             return 0
+
+    def cookie_length(self, domain):
+        '''Return approximate length of all cookie key-values for a domain.'''
+        cookies = self.cookie_jar._cookies
+
+        if domain not in cookies:
+            return 0
+
+        length = 0
+
+        for path in cookies[domain]:
+            for name in cookies[domain][path]:
+                cookie = cookies[domain][path][name]
+                length += len(path) + len(name) + len(cookie.value)
+
+        return length
 
 
 class RelaxedMozillaCookieJar(MozillaCookieJar):

commit 2b03864d6865d3cba5116f259ed6e56e12ee5d76

cookie: Handle cookies without "=".
Fix regression introduced in 38a4152. Closes chfoo/wpull#206

wpull/cookie.py
@@ -25,7 +25,7 @@ class DeFactoCookiePolicy(DefaultCookiePolicy):
 
         new_cookie_length = (self.cookie_length(cookie.domain) +
                              len(cookie.path) + len(cookie.name) +
-                             len(cookie.value))
+                             len(cookie.value or ''))
 
         if new_cookie_length >= 4100:
             return False
@@ -65,7 +65,7 @@ class DeFactoCookiePolicy(DefaultCookiePolicy):
         for path in cookies[domain]:
             for name in cookies[domain][path]:
                 cookie = cookies[domain][path][name]
-                length += len(path) + len(name) + len(cookie.value)
+                length += len(path) + len(name) + len(cookie.value or '')
 
         return length
 

wpull/app_test.py
@@ -1003,7 +1003,7 @@ class TestAppBad(BadAppTestCase):
 
         cookies = list(builder.factory['CookieJar'])
         _logger.debug('{0}'.format(cookies))
-        self.assertEqual(2, len(cookies))
+        self.assertEqual(3, len(cookies))
 
     @wpull.testing.async.async_test(timeout=DEFAULT_TIMEOUT)
     def test_long_cookie(self):

wpull/cookie_test.py
@@ -117,3 +117,22 @@ class TestCookie(unittest.TestCase):
         print(cookie_jar._cookies)
 
         self.assertFalse(cookie_jar._cookies.get('example.com'))
+
+    def test_empty_value(self):
+        cookie_jar = CookieJar()
+        policy = DeFactoCookiePolicy(cookie_jar=cookie_jar)
+        cookie_jar.set_policy(policy)
+
+        request = urllib.request.Request('http://example.com/')
+        response = FakeResponse(
+            [
+                'Set-Cookie: k'
+            ],
+            'http://example.com/'
+        )
+
+        cookie_jar.extract_cookies(response, request)
+
+        print(cookie_jar._cookies)
+
+        self.assertTrue(cookie_jar._cookies.get('example.com'))

wpull/testing/badapp.py
@@ -462,6 +462,7 @@ class Handler(http.server.BaseHTTPRequestHandler):
             'Set-cookie',
             '\x00?#?+:%ff=hope you have cookies enabled!; expires=Dog'
         )
+        self.send_header('Set-cookie', 'COOKIES')
         self.send_header('Set-cookie', 'test=valid')
         self.send_header('Content-length', '0')
         self.end_headers()