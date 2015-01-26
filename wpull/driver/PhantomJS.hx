0ee6b1989ac21dc5a42c09828c6a937966ba05ab
Christopher Foo <chris.foo@gmail.com>
2015-01-26 17:21:13 -0500

    Revert "driver: Wait before rendering snapshots"
    This reverts commit d547837b88dc0c1b5e585dc0ae5f339dd418270e.

--- wpull/driver/PhantomJS.hx
+++ wpull/driver/PhantomJS.hx
@@ -359,7 +359,7 @@ class PhantomJS {
             setPagePosition(0, 0);
             sendKey(page.event.key.Home);
 
-            Browser.window.setTimeout(loadFinishedCallback2, scrollDelay);
+            loadFinishedCallback2();
         }
 
         function actualScroll() {
--- wpull/driver/phantomjs.js
+++ wpull/driver/phantomjs.js
@@ -194,7 +194,7 @@ PhantomJS.prototype = {
 			_g.logAction("set_scroll_top",0);
 			_g.setPagePosition(0,0);
 			_g.sendKey(_g.page.event.key.Home);
-			window.setTimeout($bind(_g,_g.loadFinishedCallback2),scrollDelay);
+			_g.loadFinishedCallback2();
 		};
 		var actualScroll;
 		var actualScroll1 = null;
