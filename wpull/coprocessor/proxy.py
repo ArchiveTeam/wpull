d59102bd708f3e4bc33c133b11e5bfe6b4fddcc1
Christopher Foo <chris.foo@gmail.com>
2015-01-26 20:34:40 -0500

    coprocessor.proxy: Add fetch logging
    re: chfoo/wpull#228

--- wpull/coprocessor/proxy.py
+++ wpull/coprocessor/proxy.py
@@ -1,3 +1,12 @@
+import gettext
+import logging
+
+from wpull.backport.logging import BraceMessage as __
+import wpull.string
+
+_logger = logging.getLogger(__name__)
+_ = gettext.gettext
+
 
 class ProxyCoprocessor(object):
     '''Proxy coprocessor.'''
@@ -5,12 +14,32 @@ class ProxyCoprocessor(object):
         self._proxy_server = proxy_server
         self._cookie_jar = cookie_jar
 
-        if cookie_jar:
-            def request_callback(request):
-                cookie_jar.add_cookie_header(request)
+        proxy_server.request_callback = self._request_callback
+        proxy_server.pre_response_callback = self._pre_response_callback
+        proxy_server.response_callback = self._response_callback
 
-            def response_callback(request, response):
-                cookie_jar.extract_cookies(response, request)
+    def _request_callback(self, request):
+        _logger.info(__(
+            _('Fetching ‘{url}’.'),
+            url=request.url_info.url
+        ))
 
-            proxy_server.request_callback = request_callback
-            proxy_server.response_callback = response_callback
+        if self._cookie_jar:
+            self._cookie_jar.add_cookie_header(request)
+
+    def _pre_response_callback(self, request, response):
+        if self._cookie_jar:
+            self._cookie_jar.extract_cookies(response, request)
+
+    def _response_callback(self, request, response):
+        _logger.info(__(
+            _('Fetched ‘{url}’: {status_code} {reason}. '
+              'Length: {content_length} [{content_type}].'),
+            url=request.url,
+            status_code=response.status_code,
+            reason=wpull.string.printable_str(response.reason),
+            content_length=wpull.string.printable_str(
+                response.fields.get('Content-Length', _('none'))),
+            content_type=wpull.string.printable_str(
+                response.fields.get('Content-Type', _('none'))),
+        ))
