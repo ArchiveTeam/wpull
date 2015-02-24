3a6b016a2163998bcd24c65151290bc69597bc07
Christopher Foo <chris.foo@gmail.com>
2015-02-24 16:41:02 -0500

    app: Add ResourceMonitor. Require psutil.

--- wpull/app.py
+++ wpull/app.py
@@ -6,6 +6,8 @@ import gettext
 import logging
 import platform
 import signal
+import collections
+import psutil
 
 from trollius import From, Return
 import trollius
@@ -284,3 +286,60 @@ class Application(HookableMixin):
             _logger.debug(__('Closing server {}', server))
             server.close()
             yield From(server.wait_closed())
+
+
+ResourceInfo = collections.namedtuple(
+    'ResourceInfoType',
+    ['path', 'free', 'limit']
+)
+'''Resource level information
+
+Attributes:
+    path (str, None): File path of the resource. ``None`` is provided for
+        memory usage.
+    free (int): Number of bytes available.
+    limit (int): Minimum bytes of the resource.
+'''
+
+
+class ResourceMonitor(object):
+    '''Monitor available resources such as disk space and memory.
+
+    Args:
+        resource_paths (list): List of paths to monitor. Recommended paths
+            include temporary directories and the current working directory.
+        min_disk (int, optional): Minimum disk space in bytes.
+        min_memory (int, optional): Minimum memory in bytes.
+    '''
+    def __init__(self, resource_paths=('/',), min_disk=10000,
+                 min_memory=10000):
+        assert not isinstance(resource_paths, str), type(resource_paths)
+
+        self._resource_paths = resource_paths
+        self._min_disk = min_disk
+        self._min_memory = min_memory
+
+    def get_info(self):
+        '''Return ResourceInfo instances.'''
+        if self._min_disk:
+            for path in self._resource_paths:
+                usage = psutil.disk_usage(path)
+
+                yield ResourceInfo(path, usage.free, self._min_disk)
+
+        if self._min_memory:
+            usage = psutil.virtual_memory()
+
+            yield ResourceInfo(None, usage.available, self._min_memory)
+
+    def check(self):
+        '''Check resource levels.
+
+         Returns:
+            None, ResourceInfo: If None is provided, no levels are exceeded.
+                Otherwise, the first ResourceInfo exceeding limits is returned.
+        '''
+
+        for info in self.get_info():
+            if info.free < info.limit:
+                return info
