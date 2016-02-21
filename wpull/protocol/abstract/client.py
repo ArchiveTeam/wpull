38b217ac092300df15f97b3b8ff8600b133d31b6
Christopher Foo <chris.foo@gmail.com>
2016-02-21 15:28:20 -0500

    abstract.client: Handle connection pool concerns in BaseSession
    Add typing.

--- wpull/protocol/abstract/client.py
+++ wpull/protocol/abstract/client.py
@@ -4,8 +4,9 @@ import asyncio
 import contextlib
 import logging
 
+from typing import Optional, Callable, TypeVar, Generic
 from wpull.errors import NetworkTimedOut
-from wpull.network.connection import ConnectionPool
+from wpull.network.pool import ConnectionPool
 
 _logger = logging.getLogger(__name__)
 
@@ -19,93 +20,41 @@ class DurationTimeout(NetworkTimedOut):
     '''Download did not complete within specified time.'''
 
 
-class BaseClient(object, metaclass=abc.ABCMeta):
-    '''Base client.'''
-    def __init__(self, connection_pool=None, recorder=None):
-        '''
-        Args:
-            connection_pool (:class:`.connection.ConnectionPool`): Connection
-                pool.
-            recorder (:class:`.recorder.BaseRecorder`): Recorder.
-            stream_factory: A function that returns a new
-                :class:`.http.stream.Stream`.
-        '''
-        if connection_pool is not None:
-            self._connection_pool = connection_pool
-        else:
-            self._connection_pool = ConnectionPool()
-
-        self._recorder = recorder
-
-    @abc.abstractmethod
-    def _session_class(self):
-        '''Return session class.'''
-        return BaseSession  # return something for code checkers
-
-    @contextlib.contextmanager
-    def session(self):
-        '''Return a new session.
-
-        Returns:
-            BaseSession.
-
-        Context manager: This function is meant be used with the ``with``
-        statement.
-        '''
-        if self._recorder:
-            context_manager = self._recorder.session()
-        else:
-            context_manager = dummy_context_manager()
-
-        with context_manager as recorder_session:
-            session = self._session_class()(
-                connection_pool=self._connection_pool,
-                recorder_session=recorder_session,
-            )
-            try:
-                yield session
-            except Exception as error:
-                if not isinstance(error, StopIteration):
-                    _logger.debug('Early close session.')
-                    session.abort()
-                    session.recycle()
-                raise
-            else:
-                session.recycle()
-
-    def close(self):
-        '''Close the connection pool and recorders.'''
-        _logger.debug('Client closing.')
-        self._connection_pool.close()
-
-        if self._recorder:
-            self._recorder.close()
-
-
 class BaseSession(object, metaclass=abc.ABCMeta):
     '''Base session.'''
-    def __init__(self, connection_pool=None, recorder_session=None):
-        assert connection_pool
+    def __init__(self, connection_pool):
         self._connection_pool = connection_pool
-        self._recorder_session = recorder_session
-        self._request = None
-        self._connection = None
+        self._connections = set()
 
-    @abc.abstractmethod
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_val and not isinstance(exc_val, StopIteration):
+            _logger.debug('Early close session.')
+            self.abort()
+
+        self.recycle()
+
     def abort(self):
         '''Terminate early and close any connections.'''
+        for connection in self._connections:
+            connection.close()
 
-    @abc.abstractmethod
     def recycle(self):
-        '''Clean up and return connection back to the pool.
+        '''Clean up and return connections back to the pool.
 
         Connections should be kept alive if supported.
         '''
 
+        for connection in self._connections:
+            self._connection_pool.no_wait_release(connection)
+
+        self._connections.clear()
+
     @asyncio.coroutine
     def _acquire_connection(self, request):
         '''Return a connection.'''
-        self._request = request
         host = request.url_info.hostname
         port = request.url_info.port
         use_ssl = request.url_info.scheme == 'https'
@@ -119,6 +68,41 @@ class BaseSession(object, metaclass=abc.ABCMeta):
             connection = yield from \
                 self._connection_pool.acquire(host, port, use_ssl)
 
-        self._connection = connection
+        self._connections.add(connection)
 
         return connection
+
+
+SessionT = TypeVar('SessionT')
+
+
+class BaseClient(Generic[SessionT], metaclass=abc.ABCMeta):
+    '''Base client.'''
+    def __init__(self, connection_pool: Optional[ConnectionPool]=None):
+        '''
+        Args:
+            connection_pool: Connection pool.
+        '''
+        if connection_pool is not None:
+            self._connection_pool = connection_pool
+        else:
+            self._connection_pool = ConnectionPool()
+
+    @abc.abstractmethod
+    def _session_class(self) -> Callable[[], SessionT]:
+        '''Return session class.'''
+        return BaseSession  # return something for code checkers
+
+    def session(self) -> SessionT:
+        '''Return a new session.'''
+        session = self._session_class()(
+            connection_pool=self._connection_pool,
+        )
+        return session
+
+    def close(self):
+        '''Close the connection pool.'''
+        _logger.debug('Client closing.')
+        self._connection_pool.close()
+
+
