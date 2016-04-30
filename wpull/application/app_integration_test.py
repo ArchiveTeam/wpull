a097b5fc69bd12fa32400568a4c9d50914a6aeb7
Christopher Foo <chris.foo@gmail.com>
2016-04-30 15:11:41 -0400

    Move options testing into options_test

--- wpull/application/app_integration_test.py
+++ wpull/application/app_integration_test.py
@@ -76,11 +76,6 @@ class TestApp(GoodAppTestCase, TempDirMixin):
         self.tear_down_temp_dir()
 
     @wpull.testing.async.async_test(timeout=DEFAULT_TIMEOUT)
-    def test_no_args(self):
-        arg_parser = AppArgumentParser(real_exit=False)
-        self.assertRaises(ValueError, arg_parser.parse_args, [])
-
-    @wpull.testing.async.async_test(timeout=DEFAULT_TIMEOUT)
     def test_one_page(self):
         arg_parser = AppArgumentParser()
         args = arg_parser.parse_args([self.get_url('/')])
@@ -389,39 +384,6 @@ class TestApp(GoodAppTestCase, TempDirMixin):
         app = builder.build()
         exit_code = yield from app.run()
         self.assertEqual(0, exit_code)
-
-    @wpull.testing.async.async_test(timeout=DEFAULT_TIMEOUT)
-    def test_app_sanity(self):
-        arg_items = [
-            ('--verbose', '--quiet'),
-            ('--timestamp', '--no-clobber'),
-            ('--inet4-only', '--inet6-only'),
-            ('--warc-file=test', '--no-clobber'),
-            ('--warc-file=test', '--timestamping'),
-            ('--warc-file=test', '--continue'),
-            ('--no-iri', '--local-encoding=shiftjis'),
-            ('--no-iri', '--remote-encoding=shiftjis'),
-        ]
-
-        for arg_item in arg_items:
-            def print_(message=None):
-                print(message)
-
-            def test_exit(status=0, message=None):
-                raise ValueError(status, message)
-
-            arg_parser = AppArgumentParser()
-            arg_parser.exit = test_exit
-            arg_parser.print_help = print_
-            arg_parser.print_usage = print_
-
-            try:
-                print(arg_item)
-                arg_parser.parse_args([self.get_url('/')] + list(arg_item))
-            except ValueError as error:
-                self.assertEqual(2, error.args[0])
-            else:
-                self.assertTrue(False)
 
     @wpull.testing.async.async_test(timeout=DEFAULT_TIMEOUT)
     def test_app_python_script_api_2(self):
