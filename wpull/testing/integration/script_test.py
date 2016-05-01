1fd3da22460ac5b6524bd62d7fd9d8dcaaa741b5
Christopher Foo <chris.foo@gmail.com>
2016-05-01 16:00:18 -0400

    Split integration tests into multiple files

---
+++ wpull/testing/integration/script_test.py
@@ -0,0 +1,107 @@
+import os
+
+from wpull.application.builder import Builder
+from wpull.application.options import AppArgumentParser
+from wpull.testing.integration.base import HTTPGoodAppTestCase
+import wpull.testing.async
+
+
+class TestScriptGoodApp(HTTPGoodAppTestCase):
+    @wpull.testing.async.async_test()
+    def test_app_python_script_api_2(self):
+        arg_parser = AppArgumentParser()
+        filename = os.path.join(os.path.dirname(__file__),
+                                'testing', 'py_hook_script2.py')
+        args = arg_parser.parse_args([
+            self.get_url('/'),
+            self.get_url('/some_page'),
+            self.get_url('/mordor'),
+            'localhost:1/wolf',
+            '--python-script', filename,
+            '--page-requisites',
+            '--reject-regex', '/post/',
+            '--wait', '12',
+            '--retry-connrefused', '--tries', '1'
+        ])
+        builder = Builder(args, unit_test=True)
+    
+        app = builder.build()
+        exit_code = yield from app.run()
+        print(list(os.walk('.')))
+    
+        self.assertEqual(42, exit_code)
+    
+        engine = builder.factory['Engine']
+        self.assertEqual(2, engine.concurrent)
+    
+        stats = builder.factory['Statistics']
+    
+        self.assertEqual(3, stats.files)
+    
+        # duration should be virtually 0 but account for slowness on travis ci
+        self.assertGreater(10.0, stats.duration)
+    
+    @wpull.testing.async.async_test()
+    def test_app_python_script_api_3(self):
+        arg_parser = AppArgumentParser()
+        filename = os.path.join(os.path.dirname(__file__),
+                                'testing', 'py_hook_script3.py')
+        args = arg_parser.parse_args([
+            self.get_url('/'),
+            self.get_url('/some_page'),
+            self.get_url('/mordor'),
+            'localhost:1/wolf',
+            '--python-script', filename,
+            '--page-requisites',
+            '--reject-regex', '/post/',
+            '--wait', '12',
+            '--retry-connrefused', '--tries', '1'
+        ])
+        builder = Builder(args, unit_test=True)
+    
+        app = builder.build()
+        exit_code = yield from app.run()
+        print(list(os.walk('.')))
+    
+        self.assertEqual(42, exit_code)
+    
+        engine = builder.factory['Engine']
+        self.assertEqual(2, engine.concurrent)
+    
+        stats = builder.factory['Statistics']
+    
+        self.assertEqual(3, stats.files)
+    
+        # duration should be virtually 0 but account for slowness on travis ci
+        self.assertGreater(10.0, stats.duration)
+    
+    @wpull.testing.async.async_test()
+    def test_app_python_script_stop(self):
+        arg_parser = AppArgumentParser()
+        filename = os.path.join(os.path.dirname(__file__),
+                                'testing', 'py_hook_script_stop.py')
+        args = arg_parser.parse_args([
+            self.get_url('/'),
+            '--python-script', filename,
+        ])
+        builder = Builder(args, unit_test=True)
+        app = builder.build()
+        exit_code = yield from app.run()
+    
+        self.assertEqual(1, exit_code)
+    
+    @wpull.testing.async.async_test()
+    def test_app_plugin_script(self):
+        arg_parser = AppArgumentParser()
+        filename = os.path.join(
+            os.path.dirname(__file__), 'testing', 'plugin_script.py'
+        )
+        args = arg_parser.parse_args([
+            self.get_url('/'),
+            '--plugin-script', filename,
+        ])
+        builder = Builder(args, unit_test=True)
+        app = builder.build()
+        exit_code = yield from app.run()
+    
+        self.assertEqual(42, exit_code)
