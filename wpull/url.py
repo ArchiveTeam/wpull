bf0f9cc8fed1451465c838131e0e017a5d99ff58
Falcon Darkstar Momot <falcon.momot@leviathansecurity.com>
2017-01-17 23:10:48 +0000

    Give hostname we choked on when raising IDN hostname exceptions in url.py

--- wpull/url.py
+++ wpull/url.py
@@ -431,7 +431,10 @@ def normalize(url, **kwargs):
 @functools.lru_cache()
 def normalize_hostname(hostname):
     '''Normalizes a hostname so that it is ASCII and valid domain name.'''
-    new_hostname = hostname.encode('idna').decode('ascii').lower()
+    try:
+        new_hostname = hostname.encode('idna').decode('ascii').lower()
+    except UnicodeError as error:
+        raise UnicodeError('Hostname {} rejected: {}'.format(hostname, error)) from error
 
     if hostname != new_hostname:
         # Check for round-trip. May raise UnicodeError
