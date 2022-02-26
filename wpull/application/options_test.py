ad7e8d188c05264f609a79ed8288f38c6b2f6d78
Óscar González Fernández <oscar.gonzalez.fernandez@bbva.com>
2022-02-26 19:48:07 +0100

    Add test case for comma-separated args

--- wpull/application/options_test.py
+++ wpull/application/options_test.py
@@ -39,3 +39,32 @@ class TestOptions(unittest.TestCase):
                 self.assertEqual(2, error.args[0])
             else:
                 self.assertTrue(False)
+
+    def test_comma_list_args(self):
+        arg_item_list = [
+            '--accept', '--reject',
+            '--domains', '--exclude-domains',
+            '--hostnames', '--exclude-hostnames',
+            '--follow-tags', '--ignore-tags',
+            '--include-directories', '--exclude-directories',
+            '--proxy-domains', '--proxy-exclude-domains',
+            '--proxy-hostnames', '--proxy-exclude-hostnames',
+            ]
+        arg_dest_list = [
+            'accept', 'reject',
+            'domains', 'exclude_domains',
+            'hostnames', 'exclude_hostnames',
+            'follow_tags', 'ignore_tags',
+            'include_directories', 'exclude_directories',
+            'proxy_domains', 'proxy_exclude_domains',
+            'proxy_hostnames', 'proxy_exclude_hostnames',
+            ]
+
+        cli_input = 'item1,item2,item3'
+        expected_value = ['item1', 'item2', 'item3']
+
+        for arg_item, arg_dest in zip(arg_item_list, arg_dest_list):
+            arg_parser = AppArgumentParser()
+
+            args = arg_parser.parse_args(['http://example.invalid'] + [arg_item, cli_input])
+            self.assertEqual(expected_value, vars(args)[arg_dest])
